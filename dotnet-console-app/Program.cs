008a6c19839a9d473851b1f6a5ce7a38a6ce8280
shkanadamane28 <shkanadamane28@gmail.com>
2026-07-13 23:39:43 +0530

    .NET configure

---
+++ dotnet-console-app/Program.cs
@@ -0,0 +1,5 @@
+using System;
+
+Console.WriteLine("Hello from the self-hosted GitHub Actions demo!");
+Console.WriteLine($"Current time: {DateTimeOffset.UtcNow:O}");
+Console.WriteLine($"Runner OS: {Environment.OSVersion}");
